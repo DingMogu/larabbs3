4221eaa0a73b09889e0ce5cfbe646b181c27f424
rui <1975261197@qq.com>
2020-01-18 06:53:48 +0000

    找回密码成功信息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +27,16 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    protected function sendResetResponse(Request $request, $response)
+    {
+     session()->flash('success','密码更新成功，你已成功登录');
+     return redirect($this->redirectPath());
+    }
+
+
 }
