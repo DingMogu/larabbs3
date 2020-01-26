17fe6dc45f37317dbf602208d18ba5a5dec9e536
rui <1975261197@qq.com>
2020-01-26 07:24:18 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,5 +32,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
 
